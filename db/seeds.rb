e7d3b2d6fe1a2f05b680c0127361967fda07e7fe
Emilie Cazaux <emilie.cazaux.dussous@gmail.com>
2019-09-04 18:38:38 +0300

    mister cocktail without front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "cucumbers")
+Ingredient.create(name: "ginger syrup")
+Ingredient.create(name: "lime")
+Ingredient.create(name: "orange zest")
+Cocktail.create(name: "Boulvardier")
